e9bb632d4545f83c783411a3acbf8e53787ab5eb
Jhon Andrade <goandradeveloper@gmail.com>
2024-09-11 08:33:34 -0500

    Configuraciones de strapi, eventos y webhooks

---
+++ src/main.ts
@@ -0,0 +1,27 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { Logger, ValidationPipe } from '@nestjs/common';
+import { envs } from './config';
+
+async function bootstrap() {
+
+  const logger = new Logger('Payments-ms')
+
+
+  const app = await NestFactory.create(AppModule, {
+    // Esta configuración es para stripe
+    rawBody: true
+  });
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+  
+  await app.listen(envs.port);
+
+  logger.log(`Payments Microservice running on port ${envs.port}`)
+}
+bootstrap();
